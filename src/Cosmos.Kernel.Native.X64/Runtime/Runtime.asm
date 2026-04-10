e64856f1141c0eccbe8b8bfdf8fef4925bc4f615
valentinbreiz <valentinbreiz@gmail.com>
2026-04-10 17:46:29 +0200

    🐛 Fix x64 math: NaN/Inf checks for trig, correct fpatan stack order, replace roundsd with frndint

--- src/Cosmos.Kernel.Native.X64/Runtime/Runtime.asm
+++ src/Cosmos.Kernel.Native.X64/Runtime/Runtime.asm
@@ -1,5 +1,5 @@
 ; x64 NativeAOT Runtime Stubs
-; EH section accessors
+; EH section accessors and math functions
 
 global get_eh_frame_start
 global get_eh_frame_end
@@ -24,6 +24,7 @@ global exp
 global log
 global log2
 global log10
+
 ; float math
 global cosf
 global sinf
@@ -36,47 +37,45 @@ global log10f
 
 section .text
 
-; void* get_eh_frame_start(void)
-; Returns pointer to start of .eh_frame section
+; =============================================================================
+; EH section accessors
+; =============================================================================
+
 get_eh_frame_start:
     lea rax, [rel __eh_frame_start]
     ret
 
-; void* get_eh_frame_end(void)
-; Returns pointer to end of .eh_frame section
 get_eh_frame_end:
     lea rax, [rel __eh_frame_end]
     ret
 
-; void* get_dotnet_eh_table_start(void)
-; Returns pointer to start of .dotnet_eh_table section
 get_dotnet_eh_table_start:
     lea rax, [rel __dotnet_eh_table_start]
     ret
 
-; void* get_dotnet_eh_table_end(void)
-; Returns pointer to end of .dotnet_eh_table section
 get_dotnet_eh_table_end:
     lea rax, [rel __dotnet_eh_table_end]
+    ret
+
+; =============================================================================
+; Helper: check if xmm0 is NaN or Inf (exponent == 0x7FF)
+; Sets ZF if NaN/Inf. Trashes rax.
+; =============================================================================
+__check_nan_inf:
+    movq rax, xmm0
+    shr rax, 52
+    and eax, 0x7FF
+    cmp eax, 0x7FF
     ret
 
 ; =============================================================================
 ; Double-precision math (x87 FPU)
 ; =============================================================================
 
-; double cos(double x)
-cos:
-    sub rsp, 8
-    movsd [rsp], xmm0
-    fld qword [rsp]
-    fcos
-    fstp qword [rsp]
-    movsd xmm0, [rsp]
-    add rsp, 8
-    ret
-
 ; double sin(double x)
 sin:
+    call __check_nan_inf
+    je .sin_nan
     sub rsp, 8
     movsd [rsp], xmm0
     fld qword [rsp]
@@ -85,86 +84,120 @@ sin:
     movsd xmm0, [rsp]
     add rsp, 8
     ret
+.sin_nan:
+    movsd xmm0, [rel __math_nan]
+    ret
 
-; double tan(double x)
-tan:
+; double cos(double x)
+cos:
+    call __check_nan_inf
+    je .cos_nan
     sub rsp, 8
     movsd [rsp], xmm0
     fld qword [rsp]
-    fptan
-    fstp st0        ; fptan pushes 1.0, we pop it to leave just the result
+    fcos
     fstp qword [rsp]
     movsd xmm0, [rsp]
     add rsp, 8
     ret
+.cos_nan:
+    movsd xmm0, [rel __math_nan]
+    ret
+
+; double tan(double x)
+tan:
+    call __check_nan_inf
+    je .tan_nan
+    sub rsp, 8
+    movsd [rsp], xmm0
+    fld qword [rsp]
+    fptan
+    fstp st0            ; pop the 1.0 fptan pushes
+    fstp qword [rsp]
+    movsd xmm0, [rsp]
+    add rsp, 8
+    ret
+.tan_nan:
+    movsd xmm0, [rel __math_nan]
+    ret
 
 ; double asin(double x)
-; asin(x) = atan2(x, sqrt(1 - x*x))
+; asin(x) = fpatan(x, sqrt(1 - x*x))
+; fpatan computes atan2(ST1, ST0)
+; We want atan2(x, sqrt(1-x*x))  => ST1=x, ST0=sqrt(1-x*x)
+; So load sqrt first, then x
 asin:
     sub rsp, 24
-    movsd [rsp], xmm0          ; save x
+    movsd [rsp], xmm0              ; save x
 
-    ; compute 1 - x*x
-    mulsd xmm0, xmm0           ; x*x
-    movsd xmm1, [rel __math_one]
-    subsd xmm1, xmm0           ; 1 - x*x
-    sqrtsd xmm1, xmm1          ; sqrt(1 - x*x)
+    ; compute sqrt(1 - x*x) in xmm
+    movsd xmm1, xmm0
+    mulsd xmm1, xmm1               ; x*x
+    movsd xmm2, [rel __math_one]
+    subsd xmm2, xmm1               ; 1 - x*x
+    sqrtsd xmm2, xmm2              ; sqrt(1 - x*x)
+    movsd [rsp+8], xmm2
 
-    ; load x and sqrt(1-x*x) to FPU, use fpatan
-    movsd [rsp+8], xmm1        ; sqrt(1-x*x)
-    fld qword [rsp+8]          ; ST0 = sqrt(1-x*x)
-    fld qword [rsp]            ; ST0 = x, ST1 = sqrt(1-x*x)
-    fpatan                     ; ST0 = atan2(x, sqrt(1-x*x)) = asin(x)
+    ; FPU: ST0=sqrt(1-x*x), then push x => ST0=x, ST1=sqrt(1-x*x)
+    fld qword [rsp+8]              ; ST0 = sqrt(1-x*x)
+    fld qword [rsp]                ; ST0 = x, ST1 = sqrt(1-x*x)
+    fpatan                          ; atan2(x, sqrt(1-x*x)) = asin(x)
     fstp qword [rsp]
     movsd xmm0, [rsp]
     add rsp, 24
     ret
 
 ; double acos(double x)
-; acos(x) = atan2(sqrt(1 - x*x), x)
+; acos(x) = fpatan(sqrt(1 - x*x), x)
+; fpatan computes atan2(ST1, ST0)
+; We want atan2(sqrt(1-x*x), x) => ST1=sqrt(1-x*x), ST0=x
+; So load x first, then sqrt
 acos:
     sub rsp, 24
-    movsd [rsp], xmm0          ; save x
+    movsd [rsp], xmm0              ; save x
 
     ; compute sqrt(1 - x*x)
-    mulsd xmm0, xmm0
-    movsd xmm1, [rel __math_one]
-    subsd xmm1, xmm0
-    sqrtsd xmm1, xmm1
+    movsd xmm1, xmm0
+    mulsd xmm1, xmm1
+    movsd xmm2, [rel __math_one]
+    subsd xmm2, xmm1
+    sqrtsd xmm2, xmm2
+    movsd [rsp+8], xmm2
 
-    ; fpatan computes atan2(ST1, ST0) = atan(ST1/ST0)
-    ; We want atan2(sqrt(1-x*x), x)
-    movsd [rsp+8], xmm1        ; sqrt(1-x*x)
-    fld qword [rsp]            ; ST0 = x
-    fld qword [rsp+8]          ; ST0 = sqrt(1-x*x), ST1 = x
-    fpatan                     ; ST0 = atan2(sqrt(1-x*x), x) = acos(x)
+    ; FPU: ST0=x, then push sqrt => ST0=sqrt(1-x*x), ST1=x
+    fld qword [rsp]                ; ST0 = x
+    fld qword [rsp+8]              ; ST0 = sqrt(1-x*x), ST1 = x
+    fpatan                          ; atan2(sqrt(1-x*x), x) = acos(x)
     fstp qword [rsp]
     movsd xmm0, [rsp]
     add rsp, 24
     ret
 
 ; double atan(double x)
-; atan(x) = fpatan(x, 1.0)
+; atan(x) = fpatan(x, 1.0) = atan2(x, 1)
+; ST1=x, ST0=1.0
 atan:
-    sub rsp, 16
+    sub rsp, 8
     movsd [rsp], xmm0
-    fld1                       ; ST0 = 1.0
-    fld qword [rsp]           ; ST0 = x, ST1 = 1.0
-    fpatan                     ; ST0 = atan2(x, 1.0) = atan(x)
+    fld1                            ; ST0 = 1.0
+    fld qword [rsp]                ; ST0 = x, ST1 = 1.0
+    fpatan                          ; atan2(x, 1.0) = atan(x)
     fstp qword [rsp]
     movsd xmm0, [rsp]
-    add rsp, 16
+    add rsp, 8
     ret
 
 ; double atan2(double y, double x)
 ; xmm0 = y, xmm1 = x
+; fpatan: atan2(ST1, ST0)
+; We want atan2(y, x) => ST1=y, ST0=x
 atan2:
     sub rsp, 16
-    movsd [rsp], xmm1          ; x
-    movsd [rsp+8], xmm0        ; y
-    fld qword [rsp]            ; ST0 = x
-    fld qword [rsp+8]          ; ST0 = y, ST1 = x
-    fpatan                     ; ST0 = atan2(y, x)
+    movsd [rsp], xmm1              ; x
+    movsd [rsp+8], xmm0            ; y
+    fld qword [rsp]                ; ST0 = x
+    fld qword [rsp+8]              ; ST0 = y, ST1 = x
+    fpatan                          ; atan2(y, x)
     fstp qword [rsp]
     movsd xmm0, [rsp]
     add rsp, 16
@@ -172,40 +205,38 @@ atan2:
 
 ; double exp(double x)
 ; exp(x) = 2^(x * log2(e))
-; Uses: fldl2e, fmul, then 2^x via f2xm1 + integer part
 exp:
-    sub rsp, 16
+    sub rsp, 8
     movsd [rsp], xmm0
-    fld qword [rsp]           ; ST0 = x
-    fldl2e                     ; ST0 = log2(e), ST1 = x
-    fmulp                      ; ST0 = x * log2(e)
+    fld qword [rsp]                ; ST0 = x
+    fldl2e                          ; ST0 = log2(e), ST1 = x
+    fmulp                           ; ST0 = x * log2(e)
 
-    ; Now compute 2^ST0
-    ; Split into integer and fractional parts
-    fld st0                    ; duplicate
-    frndint                    ; ST0 = round(x*log2e) = int part
-    fsub st1, st0              ; ST1 = fractional part
-    fxch st1                   ; ST0 = frac, ST1 = int
-    f2xm1                      ; ST0 = 2^frac - 1
+    ; 2^ST0: split into int + frac
+    fld st0                         ; dup
+    frndint                         ; ST0 = int part
+    fsub st1, st0                   ; ST1 = frac part
+    fxch st1                        ; ST0 = frac, ST1 = int
+    f2xm1                           ; ST0 = 2^frac - 1
     fld1
-    faddp                      ; ST0 = 2^frac
-    fscale                     ; ST0 = 2^frac * 2^int = 2^(x*log2e) = exp(x)
-    fstp st1                   ; clean up ST1
+    faddp                           ; ST0 = 2^frac
+    fscale                          ; ST0 = 2^frac * 2^int = exp(x)
+    fstp st1                        ; pop int
     fstp qword [rsp]
     movsd xmm0, [rsp]
-    add rsp, 16
+    add rsp, 8
     ret
 
-; double log(double x)  -- natural log
+; double log(double x) -- natural log
 ; log(x) = ln(2) * log2(x)
 log:
     sub rsp, 8
     movsd [rsp], xmm0
-    fld1                       ; ST0 = 1.0
-    fld qword [rsp]           ; ST0 = x, ST1 = 1.0
-    fyl2x                      ; ST0 = 1.0 * log2(x)
-    fldln2                     ; ST0 = ln(2), ST1 = log2(x)
-    fmulp                      ; ST0 = ln(2) * log2(x) = ln(x)
+    fld1
+    fld qword [rsp]
+    fyl2x                           ; log2(x)
+    fldln2                          ; ln(2)
+    fmulp                           ; ln(2) * log2(x) = ln(x)
     fstp qword [rsp]
     movsd xmm0, [rsp]
     add rsp, 8
@@ -215,9 +246,9 @@ log:
 log2:
     sub rsp, 8
     movsd [rsp], xmm0
-    fld1                       ; ST0 = 1.0
-    fld qword [rsp]           ; ST0 = x, ST1 = 1.0
-    fyl2x                      ; ST0 = 1.0 * log2(x) = log2(x)
+    fld1
+    fld qword [rsp]
+    fyl2x                           ; 1.0 * log2(x) = log2(x)
     fstp qword [rsp]
     movsd xmm0, [rsp]
     add rsp, 8
@@ -228,25 +259,24 @@ log2:
 log10:
     sub rsp, 8
     movsd [rsp], xmm0
-    fld1                       ; ST0 = 1.0
-    fld qword [rsp]           ; ST0 = x, ST1 = 1.0
-    fyl2x                      ; ST0 = log2(x)
-    fldlg2                     ; ST0 = log10(2), ST1 = log2(x)
-    fmulp                      ; ST0 = log10(2) * log2(x) = log10(x)
+    fld1
+    fld qword [rsp]
+    fyl2x                           ; log2(x)
+    fldlg2                          ; log10(2)
+    fmulp                           ; log10(2) * log2(x) = log10(x)
     fstp qword [rsp]
     movsd xmm0, [rsp]
     add rsp, 8
     ret
 
 ; double pow(double x, double y)
-; pow(x, y) = 2^(y * log2(x))
 ; xmm0 = x, xmm1 = y
 pow:
     sub rsp, 16
-    movsd [rsp], xmm0          ; x
-    movsd [rsp+8], xmm1        ; y
+    movsd [rsp], xmm0              ; x
+    movsd [rsp+8], xmm1            ; y
 
-    ; Check y == 0 -> return 1.0
+    ; y == 0 -> return 1.0
     xorpd xmm2, xmm2
     ucomisd xmm1, xmm2
     jne .pow_nonzero_exp
@@ -256,11 +286,11 @@ pow:
     ret
 
 .pow_nonzero_exp:
-    ; Check x == 0
+    ; x == 0
     ucomisd xmm0, xmm2
-    jne .pow_normal
-    jp .pow_normal
-    ; x == 0: if y > 0 return 0, if y < 0 return +inf
+    jne .pow_nonzero_base
+    jp .pow_nonzero_base
+    ; x == 0: y > 0 -> 0, y < 0 -> +inf
     ucomisd xmm1, xmm2
     ja .pow_ret_zero
     movsd xmm0, [rel __math_pos_inf]
@@ -271,22 +301,24 @@ pow:
     add rsp, 16
     ret
 
-.pow_normal:
-    ; Check if x is negative
+.pow_nonzero_base:
+    ; x < 0?
     ucomisd xmm0, xmm2
     ja .pow_positive_base
 
-    ; Negative base: check if y is integer
-    movsd xmm3, xmm1
-    roundsd xmm4, xmm3, 3      ; truncate y
-    ucomisd xmm3, xmm4
+    ; Negative base: check if y is integer using FPU (no SSE4.1 roundsd)
+    fld qword [rsp+8]              ; ST0 = y
+    fld st0                         ; dup y
+    frndint                         ; ST0 = round(y)
+    fcomip st0, st1                 ; compare round(y) vs y, pop round(y)
+    fstp st0                        ; pop y
     jne .pow_ret_nan
     jp .pow_ret_nan
 
-    ; y is integer, negate x and compute, then fix sign
-    movsd xmm5, xmm0
-    movsd xmm0, [rel __math_neg_one]
-    mulsd xmm0, xmm5           ; x = -x (now positive)
+    ; y is integer: negate x, compute, fix sign
+    movsd xmm0, xmm0
+    movsd xmm3, [rel __math_neg_one]
+    mulsd xmm0, xmm3               ; x = -x (positive)
     movsd [rsp], xmm0
 
     ; Check if y is odd
@@ -294,10 +326,10 @@ pow:
     test rax, 1
     jz .pow_even_exp
 
-    ; Odd exponent: result is negative
-    fld qword [rsp+8]          ; y
-    fld qword [rsp]            ; |x|
-    fyl2x                      ; y * log2(|x|)
+    ; Odd exponent
+    fld qword [rsp+8]
+    fld qword [rsp]
+    fyl2x
     fld st0
     frndint
     fsub st1, st0
@@ -307,16 +339,15 @@ pow:
     faddp
     fscale
     fstp st1
-    fchs                       ; negate result
+    fchs                            ; negate result
     fstp qword [rsp]
     movsd xmm0, [rsp]
     add rsp, 16
     ret
 
 .pow_even_exp:
-    ; Even exponent: result is positive
-    fld qword [rsp+8]          ; y
-    fld qword [rsp]            ; |x|
+    fld qword [rsp+8]
+    fld qword [rsp]
     fyl2x
     fld st0
     frndint
@@ -339,17 +370,17 @@ pow:
 
 .pow_positive_base:
     ; pow(x,y) = 2^(y * log2(x))
-    fld qword [rsp+8]          ; ST0 = y
-    fld qword [rsp]            ; ST0 = x, ST1 = y
-    fyl2x                      ; ST0 = y * log2(x)
+    fld qword [rsp+8]              ; y
+    fld qword [rsp]                ; x
+    fyl2x                           ; y * log2(x)
     fld st0
-    frndint                    ; ST0 = int part
-    fsub st1, st0              ; ST1 = frac part
-    fxch st1                   ; ST0 = frac, ST1 = int
-    f2xm1                      ; ST0 = 2^frac - 1
+    frndint
+    fsub st1, st0
+    fxch st1
+    f2xm1
     fld1
-    faddp                      ; ST0 = 2^frac
-    fscale                     ; ST0 = 2^frac * 2^int
+    faddp
+    fscale
     fstp st1
     fstp qword [rsp]
     movsd xmm0, [rsp]
@@ -357,59 +388,51 @@ pow:
     ret
 
 ; =============================================================================
-; Single-precision float math (wrappers calling double versions)
+; Single-precision float wrappers (promote -> call double -> demote)
 ; =============================================================================
 
-; float sinf(float x)
 sinf:
     cvtss2sd xmm0, xmm0
     call sin
     cvtsd2ss xmm0, xmm0
     ret
 
-; float cosf(float x)
 cosf:
     cvtss2sd xmm0, xmm0
     call cos
     cvtsd2ss xmm0, xmm0
     ret
 
-; float tanf(float x)
 tanf:
     cvtss2sd xmm0, xmm0
     call tan
     cvtsd2ss xmm0, xmm0
     ret
 
-; float expf(float x)
 expf:
     cvtss2sd xmm0, xmm0
     call exp
     cvtsd2ss xmm0, xmm0
     ret
 
-; float logf(float x)
 logf:
     cvtss2sd xmm0, xmm0
     call log
     cvtsd2ss xmm0, xmm0
     ret
 
-; float log2f(float x)
 log2f:
     cvtss2sd xmm0, xmm0
     call log2
     cvtsd2ss xmm0, xmm0
     ret
 
-; float log10f(float x)
 log10f:
     cvtss2sd xmm0, xmm0
     call log10
     cvtsd2ss xmm0, xmm0
     ret
 
-; float powf(float x, float y)
 powf:
     cvtss2sd xmm0, xmm0
     cvtss2sd xmm1, xmm1
@@ -418,11 +441,11 @@ powf:
     ret
 
 ; =============================================================================
-; Constants (read-only data)
+; Constants
 ; =============================================================================
 section .rodata
 align 16
 __math_one:       dq 1.0
 __math_neg_one:   dq -1.0
-__math_nan:       dq 0x7FF8000000000000  ; canonical NaN
-__math_pos_inf:   dq 0x7FF0000000000000  ; +infinity
+__math_nan:       dq 0x7FF8000000000000
+__math_pos_inf:   dq 0x7FF0000000000000
