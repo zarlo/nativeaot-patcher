9f1e996770dec6beb1c23de89261eead165be4e3
Guillermo Santos <santosguillermo360@outlook.com>
2026-04-20 02:20:12 +0000

    Add required libSystem.Native stubs

---
+++ src/Cosmos.Kernel.Core/Bridge/Interop/libSystemNative.cs
@@ -0,0 +1,28 @@
+using System.Runtime.InteropServices;
+
+namespace Cosmos.Kernel.Core.Bridge.Export;
+
+internal static class libSystemNative
+{
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct ProcessCpuInformation
+    {
+        internal ulong lastRecordedCurrentTime;
+        internal ulong lastRecordedKernelTime;
+        internal ulong lastRecordedUserTime;
+    }
+
+    [UnmanagedCallersOnly(EntryPoint = "SystemNative_GetCpuUtilization")]
+    internal static unsafe double SystemNative_GetCpuUtilization(ProcessCpuInformation* previousCpuInfo)
+    {
+        // TODO: Implement actual CPU utilization calculation.
+        return 50.0;
+    }
+    
+    [UnmanagedCallersOnly(EntryPoint = "SystemNative_SchedGetCpu")]
+    internal static int SystemNative_SchedGetCpu()
+    {
+        // TODO: Implement actual CPU retrieval logic.
+        return 0;
+    }
+}
